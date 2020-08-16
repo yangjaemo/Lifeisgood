25a755eccadd1c8a19ce31dd41327d813a318f58
yangjaemo <gosu0118@gmail.com>
2020-08-16 22:37:00 +0900

    yangjaemo
    kakao

--- app/src/main/java/com/example/lifeisgood/MainActivity.kt
+++ app/src/main/java/com/example/lifeisgood/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.lifeisgood
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import java.security.MessageDigest
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -15,10 +21,26 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var gender: TextView
     private lateinit var btnLogout: Button
 
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
         setContentView(R.layout.activity_main)
+        try {
+            val info = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
+            val signatures = info.signingInfo.apkContentsSigners
+            val md = MessageDigest.getInstance("SHA")
+            for (signature in signatures) {
+                val md: MessageDigest
+                md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val key = String(Base64.encode(md.digest(), 0))
+                Log.d("Hash key:", "!!!!!!!$key!!!!!!")
+            }
+        } catch(e: Exception) {
+            Log.e("name not found", e.toString())
+        }
+
         if (SharedPrefManager.getInstance(this).isLoggedIn) {
             id = findViewById(R.id.textViewId)
             userName = findViewById(R.id.textViewUsername)
@@ -47,4 +69,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             SharedPrefManager.getInstance(applicationContext).logout()
         }
     }
+
+
+
+
 }
